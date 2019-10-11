3e362e9ec5a0507d7f79986685ba8f0d3099d7f2
Revor Jan Nuiamäe <rnuiame@tlu.ee>
2019-10-11 16:33:06 +0300

    Lab3: React hello world

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-//const homepage = require("./homepage.js");
-//const itempage = require("./itempage.js");
-import homepage from "./homepage.js";
-import itempage from "./itempage.js";
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log("i am index file");
+const root = document.getElementById("app");
 
-window.addEventListener("load", () => {
-    homepage.setup();
-    itempage.setup();
-});
+ReactDOM.render(
+    React.createElement(
+        "button",
+        {},
+        "I am a button, Hello world"
+    ),
+    root
+);
