80bacd45d2c3045fede3de3122bf2b3f24f44315
Revor Jan Nuiamäe <rnuiame@tlu.ee>
2019-10-11 20:17:27 +0300

    Lab3: Mainpage React

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return(
+        <div class="header">
+            <img class="header-logo" src="images/TLU_logo.png"/>
+            <div class="header-buttons">
+                <button>Login/Signup</button>
+                <button>Cart</button>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div class="header">
+            <img class="header-logo" src="images/TLU_logo.png"/>
+            <div class="header-buttons">
+                <button>Login/Signup</button>
+                <button>Cart</button>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
